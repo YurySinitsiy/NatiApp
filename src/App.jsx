a48232fc489043a1db9878bf477728318a567c63
Yury Sinitskiy <unstable195@gmail.com>
2025-07-29 10:16:56 +0300

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Registration from "./pages/Registration";
 import ForgotPassword from "./pages/ForgotPassword";
 import UpdatePassword from "./pages/UpdatePassword";
 import Users from "./pages/Users";
+import ProtectedRoute from "./components/ProtectedRoute";
+import AuthLayout from "./components/AuthLayout"; 
 
 const theme = createTheme({
   palette: {
@@ -18,12 +20,19 @@ function App() {
     <ThemeProvider theme={theme}>
       <BrowserRouter>
         <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Registration />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/update-password" element={<UpdatePassword />} />
-          <Route path="/" element={<Users />} />
-          <Route path="/main" element={<Users />} />
+
+          <Route element={<AuthLayout />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Registration />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="/update-password" element={<UpdatePassword />} />
+          </Route>
+
+          <Route element={<ProtectedRoute />}>
+            <Route path="/" element={<Users />} />
+            <Route path="/main" element={<Users />} />
+          </Route>
+
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </BrowserRouter>
